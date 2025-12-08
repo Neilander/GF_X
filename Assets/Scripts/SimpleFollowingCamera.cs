e4e96586399bcd44ba9e51cea92e3247871cc7bd
Meiyou <1139964041@qq.com>
2025-12-08 21:42:26 +0800

    新建地形

---
+++ Assets/Scripts/SimpleFollowingCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFollowingCamera : MonoBehaviour
+{
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z);
+    }
+}
