42bd6ad23945ca3d8c40d47971940b5e1c461b54
Meiyou <1139964041@qq.com>
2026-04-12 12:53:28 +0800

    相机视角改为正交，英雄wasd移动方向适配相机角度

--- Assets/AAAGame/Scripts/PlayerControl/InputDirTranslator.cs
+++ Assets/AAAGame/Scripts/PlayerControl/InputDirTranslator.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class InputDirTranslator 
+public class InputDirTranslator
 {
     public static Vector2 Translate(Vector2 inputData)
     {
-        Vector2 returnDir = inputData;
+        if (inputData.sqrMagnitude <= 0.0001f)
+        {
+            return Vector2.zero;
+        }
 
+        Camera cam = Camera.main;
+        if (cam == null)
+        {
+            return inputData;
+        }
 
-        return returnDir;
+        float yaw = cam.transform.eulerAngles.y;
+        Vector3 worldDir = Quaternion.Euler(0f, yaw, 0f) * new Vector3(inputData.x, 0f, inputData.y);
+        worldDir = Vector3.ClampMagnitude(worldDir, 1f);
+        return new Vector2(worldDir.x, worldDir.z);
     }
 }
