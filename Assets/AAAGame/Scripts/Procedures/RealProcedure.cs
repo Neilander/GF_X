a5a62734f34ace6cb350539bae95284036f17962
meiyou <1139964041@qq.com>
2026-04-18 16:45:55 +0800

    制作前2关地图，其中建筑需要等美术出

--- Assets/AAAGame/Scripts/Procedures/RealProcedure.cs
+++ Assets/AAAGame/Scripts/Procedures/RealProcedure.cs
@@ -11,7 +11,7 @@ public class RealProcedure : ProcedureBase
     protected override void OnEnter(IFsm<IProcedureManager> procedureOwner)
     {
         base.OnEnter(procedureOwner);
-        GameEntry.GetComponent<GeneralSetup>().GeneralSystemSetup("Lv_1");
+        GameEntry.GetComponent<GeneralSetup>().GeneralSystemSetup("Lv_2");
 
         GF.UI.OpenUIForm(UIViews.ResourceModifyBar);
         // 加载阶段切换按钮
